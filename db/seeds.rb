86e6be7267e33801c7bbe3a835cbbdc75be57e28
canpan14 <sammachlin@hotmail.com>
2017-10-19 10:50:23 -0400

    Changed levels to give exp needed for next level

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Level.create(number: 1, exp_needed: 0, attack: 6, health: 20)
-(2..20).each do |l|
+(1..19).each do |l|
   Level.create(number: l,
                exp_needed: 10 * l,
                attack: 5 + l,
                health: 20 * l)
 end
+Level.create(number: 20, exp_needed: nil, attack: 25, health: 400)
 
 Enemy.create(name: 'Goblin', level_id: 1, attack_chance: 0.5, dodge_chance: 0.5,
              predict_dodge_chance: 0.0)
